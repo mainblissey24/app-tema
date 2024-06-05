ac1d0e99d774a9a4e227ddd3a16011f8163d98ea
Patrick <115029356+mainblissey24@users.noreply.github.com>
2024-06-05 14:49:21 -0300

    app-thema

---
+++ src/config/theme.jsx
@@ -0,0 +1,21 @@
+import { DarkTheme } from "@react-navigation/native";
+import { DefaultTheme, MD3DarkTheme, MD3LightTheme } from "react-native-paper";
+
+
+
+const ThemeDark ={
+    ...MD3DarkTheme
+}
+
+const ThemeDarkNavigation={
+    ...DarkTheme,
+}
+
+const ThemeLight={
+    ...MD3LightTheme
+}
+
+const ThemeLightNavigation ={
+    ...DefaultTheme
+}
+export {ThemeDark, ThemeLight, ThemeDarkNavigation, ThemeLightNavigation}
